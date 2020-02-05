096fee175137140dd3f126c94b9ed54df486909e
Taha Yazidi <yazi.taha@gmail.com>
2020-02-05 19:47:56 +0000

    DoRK

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *
  * main
  *
  * Return: Always 1
@@ -9,6 +8,6 @@
 int main(void)
 {
 write(2, "and that piece of arts is useful\"
-- Dora Korpar, 2015-10-19\n", 59);
+- Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
