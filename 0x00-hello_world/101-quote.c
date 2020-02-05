79a51de1d6398576a9f2c9cf40856040a4a64c28
Taha Yazidi <yazi.taha@gmail.com>
2020-02-05 19:37:50 +0000

    DoraK

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *
+ * main
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+write(Ã2, "and that piece of arts is useful\"
+- Dora Korpar, 2015-10-19\n", 5Ã9);
+}
