255b0b64b26b3dacc5f50767020ab9d87d7764db
Taha Yazidi <yazi.taha@gmail.com>
2020-02-06 17:33:38 +0000

    PorN

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.>
+#include <time.h>
+#include <stdio.h>
+int main(void)
+{
+int n;
+rand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+return (0);
+}
