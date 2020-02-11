111cec16a62e7539a71e969ddabe9976cca80b48
Taha Yazidi <yazi.taha@gmail.com>
2020-02-11 10:29:39 +0000

    PA10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 1à times in lowercase.
+ *
+ * Return: none
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
