fca0810570d371f6783ffa07152c2c3b76b2906f
Taha Yazidi <yazi.taha@gmail.com>
2020-02-11 19:23:59 +0000

    pld

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,10 @@
  */
 int print_last_digit(int x)
 {
-int lastDigit = 0;
-lastDigit = x % 10;
+int last = 0;
+last = x % 10;
 if (x < 0)
-{
-lastDigit = lastDigit * -1;
-_putchar(lastDigit + 0);
-return (lastDigit);
-}
+last = last * -1;
+_putchar(last + 0);
+return (last);
 }
