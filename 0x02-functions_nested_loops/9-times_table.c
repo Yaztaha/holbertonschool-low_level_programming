cf81ff0153b3a0b989e1a37ee5dd99d884b19294
Taha Yazidi <yazi.taha@gmail.com>
2020-02-11 20:12:29 +0000

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ *
+ * times_table - prints the nine time table.
+ *
+ * Return: void.
+ *
+ */
+void times_table(void)
+{
+int x, y res;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+res = (x * y);
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && y != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
+}
