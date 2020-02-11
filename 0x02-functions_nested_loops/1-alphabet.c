640c1f88a1b647c3b5874a6295c6bcf2013bed1d
Taha Yazidi <yazi.taha@gmail.com>
2020-02-11 10:09:14 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - prints the alphabet in lowercase followed by a newline.
+ *
+ * Return: None.
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
