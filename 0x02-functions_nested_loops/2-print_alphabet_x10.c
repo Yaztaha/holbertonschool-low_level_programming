cde60d492ccf81c4ffdd62a6b5d7705a419705dd
Taha Yazidi <yazi.taha@gmail.com>
2020-02-12 17:54:41 +0000

    pa10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char x;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
