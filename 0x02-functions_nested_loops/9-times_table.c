46643d36b160b1f4dd2510d2aae373fa2a1be129
Taha Yazidi <yazi.taha@gmail.com>
2020-02-12 09:36:35 +0000

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,34 +4,38 @@
  * Return: void.
  */
 void times_table(void)
+
 {
-int x;
-int y;
-for (x = 0; x < 10; x++)
-{
-for (y = 0; y < 10; y++)
-{
-int times = x * y;
-if (times < 10)
-{
-if (y > 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-_putchar(time + '0');
-}
-else
-{
-_putchar(' ');
-_putchar((times / 10) + '0');
-_putchar((times % 10) + '0');
-}
-if (y < 9)
-{
-_putchar(',');
-}
-}
-_putchar('\n');
-}
+
+	int x;
+	int y;
+	int times;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			times = x * y;
+			if (times < 10)
+			{
+				if (y > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(times + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((times / 10) + '0');
+				_putchar((times % 10) + '0');
+			}
+			if (y < 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
