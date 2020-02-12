9e526aa85be193488d86c3a6e76d335fb00719bc
Taha Yazidi <yazi.taha@gmail.com>
2020-02-12 16:36:29 +0000

    pto98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,27 @@
  */
 void print_to_98(int n)
 {
-int x;
-if (n < 98)
+if (n > 98)
 {
-for (x = n; x <= 98; x++)
-if (x != 98)
-printf("%x, ", x);
-else
-printf("%x\n", x);
+	for (n = n; n >= 98; n--)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
 }
 else
 {
-for (x = n; x >= 98; x--)
-if (x != 98)
-printf("%x, ", x);
-else
-printf("%x\n", x);
+	for (n = n; n <= 98; n++)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
 }
+printf("\n");
 }
