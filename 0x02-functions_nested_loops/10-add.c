66daf3087faeb231c48cb99607bc913f72a57f08
Taha Yazidi <yazi.taha@gmail.com>
2020-02-12 10:02:36 +0000

    addtwointegers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adds two integers.
+ *
+ * @x: 1st integer.
+ * @y: 2nd integer.
+ * Return: result of the addition.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
