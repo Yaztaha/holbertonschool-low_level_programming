4464641b68a8c67aec56c14b2516fe116d3702ee
Taha Yazidi <yazi.taha@gmail.com>
2020-02-13 12:47:32 +0000

    pn

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints numbers.
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
