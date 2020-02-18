18ae74e7407af0d7b3facabdb92a8d1fa6724363
Taha Yazidi <yazi.taha@gmail.com>
2020-02-18 17:31:11 +0000

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <string.h>
 /**
  * rev_string - print string in reverse order.
  * @s : string to print.
@@ -7,16 +6,18 @@
  */
 void rev_string(char *s)
 {
-	char *a, *b;
+	char a = s[0];
+	int b = 0;
+	int i;
 
-	a = s;
-	b = s + strlen(s) - 1;
-	while (a < b)
+	while (s[b] != '\0')
+		b++;
+
+	for (i = 0; i < b; i++)
 	{
-		char temp = *a;
-		*a = *b;
-		*b = temp;
-		a++;
 		b--;
+		a = s[i];
+		s[i] = s[b];
+		s[b] = a;
 	}
 }
