dc0cbcfc0e8c2134ec775c51fcef1cead4aa281a
Taha Yazidi <yazi.taha@gmail.com>
2020-02-21 17:19:45 +0000

    comparetwostr

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings.
+ * @s1: string.
+ * @s2: string.
+ * Return: difference between two characters.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+		;
+	return (s1[i] - s2[i]);
+}
