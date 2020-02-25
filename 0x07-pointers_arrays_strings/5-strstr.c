5d90ff3f70d3ed1a6e6386d47ba856d064ac821d
Taha Yazidi <yazi.taha@gmail.com>
2020-02-25 19:44:46 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strstr - find the first occurence of substring in string ;
+ * @haystack: string.
+ * @needle: substring.
+ * Return: 0.
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *a, *b;
+
+	b = needle;
+	if (*b == 0)
+		return (haystack);
+
+	for (; *haystack != 0; haystack += 1)
+	{
+		if (*haystack != *b)
+			continue;
+		a = haystack;
+		while (1)
+		{
+			if (*b == 0)
+				return (haystack);
+			if (*a++ != *b++)
+				break;
+		}
+		b = needle;
+	}
+	return (0);
+}
