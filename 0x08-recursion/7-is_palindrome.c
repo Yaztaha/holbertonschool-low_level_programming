decb3963930ae63db2a800e0b6485a80dec40609
Taha Yazidi <yazi.taha@gmail.com>
2020-02-27 19:48:27 +0000

    is_palindrome

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - string length.
+ * @s: string.
+ *
+ * Return: string l.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
+/**
+ * pal - strings comparison.
+ * @s: pointer.
+ * @a: value a.
+ * @b: value b.
+ */
+int pal(char *s, int a, int b)
+{
+	if (a < b)
+	{
+		if (s[a] == s[b])
+			return (pal(s, a + 1, b - 1));
+		else if (s[a] != s[b])
+			return (0);
+	}
+	return (1);
+}
+/**
+ * is_palindrome - checks if it is palindrome.
+ * @s: pointer.
+ * Return: 1 if true 0 if false.
+ */
+int is_palindrome(char *s)
+{
+	return (pal(s, 0, _strlen_recursion(s) - 1));
+}
