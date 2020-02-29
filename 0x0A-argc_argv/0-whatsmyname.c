8cf0a5c03a26404f5495a7fdf413d89753505160
Taha Yazidi <yazi.taha@gmail.com>
2020-02-29 17:37:24 +0000

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program.
+ * @argc: count.
+ * @argv: array.
+ * Return: 0.
+ */
+int  main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
