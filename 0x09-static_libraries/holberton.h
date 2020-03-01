c635a539bfafb2009201263c6d731a9f1cbcdedc
Taha Yazidi <yazi.taha@gmail.com>
2020-03-01 10:59:42 +0000

    update

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,25 +1,25 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _isupper(int);
-char *_memset(char *, char *);
-char *_strcat(char *, char *);
-int _isdigit(int);
-char *_memcpy(char *, char *, unsigned int);
-char *_strncat(char *, char *, int);
-int _atoi(char *);
-char *_strchr(char *, char *);
-int _strlen(char *);
-char *_strncpy(char *, char *, int);
-int _islower(int);
-int _putchar(char);
-void _puts(char *);
-int _strcmp(char *, char *);
-unsigned int _strspn(char *, char *);
-int _isalpha(int);
-char *_strpbrk(char *, char *);
-char *_strstr(char *, char *);
-int _abs(int);
-char *_strcpy(char *, char *);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
