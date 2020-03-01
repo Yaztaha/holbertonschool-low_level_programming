5d37c05804c213f483137981bef87a7cd73f2e0f
Taha Yazidi <yazi.taha@gmail.com>
2020-03-01 10:54:52 +0000

    staticlib

---
+++ 0x09-static_libraries/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _isupper(int);
+char *_memset(char *, char *);
+char *_strcat(char *, char *);
+int _isdigit(int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strncat(char *, char *, int);
+int _atoi(char *);
+char *_strchr(char *, char *);
+int _strlen(char *);
+char *_strncpy(char *, char *, int);
+int _islower(int);
+int _putchar(char);
+void _puts(char *);
+int _strcmp(char *, char *);
+unsigned int _strspn(char *, char *);
+int _isalpha(int);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+int _abs(int);
+char *_strcpy(char *, char *);
+
+#endif
