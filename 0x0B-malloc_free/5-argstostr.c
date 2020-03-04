c675fed2deff06c69107337795f7d3ef47d92386
Taha Yazidi <yazi.taha@gmail.com>
2020-03-04 17:10:31 +0000

    argstostr

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -26,7 +26,7 @@ char *argstostr(int ac, char **av)
 			length++;
 		length++;
 	}
-	s = malloc(sizeof(char) * length);
+	s = malloc(sizeof(char) * length + 1);
 	if (s == NULL)
 	{
 		return (NULL);
