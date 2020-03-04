b0eebf704347f0bba0e76c49b3836b5776689ed5
Taha Yazidi <yazi.taha@gmail.com>
2020-03-04 17:02:38 +0000

    argstostr

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates arguments.
+ * @ac: argc.
+ * @av: argv.
+ * Return: concatenated arguements.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int length = 0;
+	char *s;
+	int a;
+	int b;
+	int x = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+	s = malloc(sizeof(char) * length);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			s[x] = av[a][b];
+		x++;
+		}
+		s[x++] = '\n';
+	}
+	return (s);
+}
