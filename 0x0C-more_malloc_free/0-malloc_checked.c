8be8ed805fbd03a136be23a4c7c7099699c44438
Taha Yazidi <yazi.taha@gmail.com>
2020-03-05 18:00:36 +0000

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - memory allocation.
+ * @b: allocated memory value.
+ * Return: pointer.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	*point;
+
+	point = malloc(b);
+
+	if (point == NULL)
+	{
+		exit(98);
+	}
+
+	return (point);
+}
