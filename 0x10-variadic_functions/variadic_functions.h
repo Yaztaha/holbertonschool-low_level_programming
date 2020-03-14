ab6d223fed9bcff4dd3197b1958cd1b7040f1444
Taha Yazidi <yazi.taha@gmail.com>
2020-03-14 13:44:40 +0000

    update-guarded

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,2 +1,14 @@
+#ifndef VAR_FUNC
+#define VAR_FUNC
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+typedef struct vf
+{
+char *vf
+void (*p)();
+} vf_t;
+
+#endif
