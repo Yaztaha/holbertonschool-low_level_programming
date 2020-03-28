9a3abceedb553c76f37d350d97a9a5244d68c913
Taha Yazidi <yazi.taha@gmail.com>
2020-03-28 18:28:41 +0000

    add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * str_len - function to determine the length of a string.
+ * @s: string pointer.
+ * Return: length of string.
+ */
+int str_len(const char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
+/**
+ * add_node - add a node to string beginning.
+ * @head: string head double pointer.
+ * @str: string to be added.
+ * Return: new string.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strLength(str);
+	new_node->next = *head;
+
+	*head = new_node;
+	return (new_node);
+}
