605df442f4dcdcdbc1a750421b3ab89ecdd9b5eb
Taha Yazidi <yazi.taha@gmail.com>
2020-03-28 19:03:25 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * premain - function that run before main.
+ */
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
