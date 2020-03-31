bf2e6e57d23c1b6f57651feb0db33b0f5d5b4541
Taha Yazidi <yazi.taha@gmail.com>
2020-03-31 21:23:03 +0000

    header_file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#define BUF_SIZE 1024
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,5 +12,7 @@
 #include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
