9fe43f2967fbb063a32d5d8a5ba51abbdd75fb18
Taha Yazidi <yazi.taha@gmail.com>
2020-05-29 12:18:02 +0000

    header_file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,6 @@ typedef struct hash_table_s
 
 /*** Task's functions ***/
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
