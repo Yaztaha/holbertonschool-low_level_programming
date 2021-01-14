d24e1f82e4545327193dbbcb93936f5cfd69050c
yaztaha <yazi.taha@gmail.com>
2021-01-14 11:14:16 +0000

    search_algos_header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/** Headers **/
+#include <stdio.h>
+/** **/
+
+/** Functions **/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+/** **/
+
+#endif
+
